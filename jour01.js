7798bb8fdc7020afb820be9e43041fe6b837aa83
Nicolas-Maillols <nicomaillols@gmail.com>
2021-12-13 14:07:58 +0100

    exo-1-2-3-4

---
+++ jour01.js
@@ -0,0 +1,23 @@
+//exo 1
+
+console.log("Hello World!");
+
+//exo 2
+
+var test = "My name is Nicolas";
+
+console.log(test);
+
+//exo 3
+
+let name = "Nicolas";
+
+console.log("Nice to meet you " + name)
+
+//exo 4
+
+let testLength = "I'm very long !"
+
+console.log(testLength)
+
+

e983817678910e0d25bdc8fe19e1e9d15f0e56ee
Nicolas-Maillols <nicomaillols@gmail.com>
2021-12-14 13:54:06 +0100

    correction-exo2

--- Jour02/jour02.js
+++ Jour02/jour02.js
@@ -4,6 +4,10 @@ const integer = 102;
 const float = 13.9;
 console.log(integer, float);
 
+//exo2
+
 let basic = 34;
-let stringifield = "34";
+let stringifield = basic.toString;
 console.log(stringifield)
+
+//exo3

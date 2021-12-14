244e91fc12f540c7f7ff8ddca97f9838ff954854
Nicolas-Maillols <nicomaillols@gmail.com>
2021-12-14 14:01:56 +0100

    exo3

--- Jour02/jour02.js
+++ Jour02/jour02.js
@@ -7,7 +7,11 @@ console.log(integer, float);
 //exo2
 
 let basic = 34;
-let stringifield = basic.toString;
-console.log(stringifield)
+let stringifield = basic.toString();
+console.log(stringifield);
 
 //exo3
+
+let num = 1.5;
+let rounded = Math.round(num);
+console.log(rounded);

cd45625046f7c21163fa3f6ca644d416056ef1b2
Nicolas-Maillols <nicomaillols@gmail.com>
2021-12-14 14:02:41 +0100

    refresh-repo

---
+++ Jour01/jour01.js
@@ -0,0 +1,60 @@
+//exo 1
+
+console.log("Hello World!");
+
+//exo 2
+
+var test = "My name is Nicolas";
+
+console.log(test);
+
+//exo 3
+
+let firstname = "Nicolas";
+
+console.log("Nice to meet you " + firstname);
+
+//exo 4
+
+let testLength = "I'm very long !";
+
+console.log(testLength);
+
+//exo5
+
+let food = "croissant is meh";
+
+food = food.replace("meh", "so good");
+
+console.log(food);
+
+//exo6
+
+let basic = "This is cool ";
+let basicUp = "basic ";
+let basicDown = "basic";
+
+console.log(basic + basicUp + basicDown);
+
+//exo7
+
+let word = "banana";
+let letters = word.split("");
+console.log(letters);
+
+//exo8
+
+let age = 26;
+let template = (`I'm ${age} years old`);
+console.log(template);
+
+//Bonus
+
+let changed = "Bonjour";
+let sel = changed.replace(/o/g, "a");
+
+console.log(sel);
+
+
+
+

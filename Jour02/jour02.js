32b3deaaa684079f9ded27ff4657b58c77984873
Nicolas-Maillols <nicomaillols@gmail.com>
2021-12-14 14:37:16 +0100

    exo4 5 6

--- Jour02/jour02.js
+++ Jour02/jour02.js
@@ -6,12 +6,39 @@ console.log(integer, float);
 
 //exo2
 
-let basic = 34;
-let stringifield = basic.toString();
+const basic = 34;
+const stringifield = basic.toString();
 console.log(stringifield);
 
 //exo3
 
-let num = 1.5;
-let rounded = Math.round(num);
+const num = 1.5;
+const rounded = Math.round(num);
 console.log(rounded);
+
+//exo4
+
+const test = 12;
+const bis = 5;
+console.log(test + bis);
+
+//exo5
+
+const teest = 143;
+const biis = 219;
+
+if(teest > biis){
+} else(console.log(false));
+
+if(teest < biis){
+} else(console.log(true));
+
+//exo6
+
+let limit = 50;
+let score = 64;
+
+if(limit <= score){
+    console.log("OK Good!");
+} else(console.log("Oh Noooooo...."));
+

4e30c5d4ed970582c4156be4ff7800790b7b6769
Nicolas-Maillols <nicomaillols@gmail.com>
2021-12-14 16:38:08 +0100

    bonus2

--- Jour02/jour02.js
+++ Jour02/jour02.js
@@ -67,5 +67,48 @@ if (random === 6) {
     console.log("Yes I Win!!");
 } else(console.log("So Close..."));
 
-//
+//BONUS2
+
+let month = "December";
+
+switch (month) {
+	case "January": 
+		console.log("Winter");
+		break;
+	case "February": 
+		console.log("winter");
+		break;
+	case "March": 
+		console.log("Winter");
+		break;
+	case "April": 
+		console.log("Spring");
+		break;
+    case "May": 
+		console.log("Spring");
+		break;
+    case "June": 
+		console.log("Spring");
+		break;
+    case "July": 
+		console.log("Summer");
+		break;
+    case "August": 
+		console.log("Summer");
+		break;
+    case "September": 
+		console.log("Summer");
+		break; 
+    case "October": 
+		console.log("Fall");
+		break;
+    case "November": 
+		console.log("Fall");
+		break;
+    case "December": 
+		console.log("Winter");
+		break;
+	default: 
+		console.log("That's not a season...");
+}
 

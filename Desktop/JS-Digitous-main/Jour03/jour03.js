2a390e7fc04a5b784dacfbdbad25bed89e1e7ad6
NickoMaill <nicomaillols@gmail.com>
2021-12-15 14:37:21 +0100

    jour03 exo1-3

---
+++ Desktop/JS-Digitous-main/Jour03/jour03.js
@@ -0,0 +1,31 @@
+//exo1
+
+const fruits = ["mango", "lemon", "blueberry"];
+console.log(fruits);
+console.table(fruits);
+
+//exo2
+
+const ingredients = ["eggs", "milk", "butter"];
+
+console.log(ingredients[1]);
+
+console.log(ingredients.indexOf("butter"));
+
+//exo3
+
+const objects = ["pen", "book", "lamp"]
+objects.unshift("chair");
+console.log(objects);
+
+objects.pop()
+console.log(objects);
+
+objects.push("laptop")
+console.log(objects);
+
+objects.shift()
+console.log(objects);
+
+//exo4
+

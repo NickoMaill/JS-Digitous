a70a5f2aab8a4fef1de0c5a318479150a2504ff5
Nicolas-Maillols <nicomaillols@gmail.com>
2022-01-03 14:51:23 +0100

    exo2-exo3

--- Revision-JS/forloop.js
+++ Revision-JS/forloop.js
@@ -4,9 +4,30 @@ console.log("Hello World!");
 
 let num = 5;
 
-for (let i = num ; i <=10; i++){
+for (let i = num ; i <=10; i++) {
     num += (Math.pow(i, 2));
 }
 console.log(num);
 
+//Exo2
 
+let multi = 0;
+
+for (let i = 100 ; i <= 1000; i++) {
+    if (i % 7) {
+        result = multi + 1;
+    };
+};
+console.log(multi + 1);
+
+
+// Exo3
+
+
+for (i = 1 ; i <= 20; i++) {
+    let dice = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    console.log(dice)
+    if (dice >= 5) {
+        console.log(dice);
+    };
+};

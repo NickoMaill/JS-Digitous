780754c761fbfc243c28fbd0eb4b1aa0ea07285c
Nicolas-Maillols <nicomaillols@gmail.com>
2022-01-04 11:56:37 +0100

    exo3While

--- Revision-JS/whileLoop.js
+++ Revision-JS/whileLoop.js
@@ -20,4 +20,19 @@ console.log(count);
 
 //Exo3
 
+let ussainBolt = Math.floor(Math.random() * (10 + 1));
+let tysonGay = Math.floor(Math.random() * (10 + 1));
 
+while (ussainBolt < 100 && tysonGay < 100){
+    ussainBolt++;
+    tysonGay++;
+    console.log(ussainBolt, tysonGay)
+    
+    if (ussainBolt === 100 && tysonGay !== 100) {
+        console.log("ussain!")
+
+    } else if (tysonGay === 100 && ussainBolt !== 100){
+        console.log("tyson!")
+    }
+
+}

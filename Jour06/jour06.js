1a0d77bf64660e103eb481e491e6b51f1c94cb4b
Nicolas-Maillols <nicomaillols@gmail.com>
2022-01-06 14:52:32 +0100

    jour06-exo2-start

--- Jour06/jour06.js
+++ Jour06/jour06.js
@@ -1,1 +1,13 @@
 console.log("Hello World!");
+
+function formatDate(strDate) {
+
+    const date = new Date(strDate);
+    return `${(date.getDay())} / ${(date.getMonth() +1)} / ${(date.getFullYear())}`
+
+}
+
+console.log(formatDate("2022-01-06"));
+
+//exo2
+

bcc9ba85d5791df31001ef2191e101ecfe9ddac9
Nicko <nickos@iMac.local>
2022-01-10 16:19:49 +0100

    Jour07-exo3-corrections

--- Jour07/Jour07.js
+++ Jour07/Jour07.js
@@ -34,60 +34,74 @@ const longNames = [
     }
 ];
 
-const shortNames = longNames.map(function(name){
-        return `${(name.firstName)} ${(name.lastName)}`
-})
+const shortNames = longNames.map(function (name) {
+    let name1 = {name: `${(name.firstName)} ${(name.lastName)}`};
+    return name1;
 
-console.table(shortNames);
+});
+
+console.log(shortNames);
 
 
 //exo4
 
 const array1 = [1, "toto", 34, "javascript", 8];
-const numbers = array1.filter(function(filt){
+const numbers = array1.filter(function (filt) {
     return filt >= 0;
-})
+});
 
 console.log(numbers);
 
 //exo5
 
 const array2 = [1, 2, 3, 4, 5, 6, 7, 8];
-const even = array2.filter(function(pair){
+const even = array2.filter(function (pair) {
     return pair % 2 - 1;
-})
+});
 
 console.log(even);
 
 //exo6
 
 const cakes = [
-	{
-		name: "cake",
-		flavor: "vanilla",
-		status: "available"
-	},
-	{
-		name: "brownie",
-		flavor: "chocolate",
-		status: "available"
-	},
-	{
-		name: "pie",
-		flavor: "strawberry",
-		status: "available"
-	},
-	{
-		name: "muffin",
-		flavor: "pistachio",
-		status: "available"
-	},
-	{
-		name: "donut",
-		flavor: "chocolate",
-		status: "available"
-	},
+    {
+        name: "cake",
+        flavor: "vanilla",
+        status: "available"
+    },
+    {
+        name: "brownie",
+        flavor: "chocolate",
+        status: "available"
+    },
+    {
+        name: "pie",
+        flavor: "strawberry",
+        status: "available"
+    },
+    {
+        name: "muffin",
+        flavor: "pistachio",
+        status: "available"
+    },
+    {
+        name: "donut",
+        flavor: "chocolate",
+        status: "available"
+    },
 ];
 
+const cakesFilt = cakes.filter(function (cakes) {
+    return cakes.flavor === "chocolate"
+});
 
+//console.log(cakesFilt);
+
+const soldOut = cakesFilt.map(function (sold) {
+    if (sold.status === "available") {
+        return `${(sold.name)} ${(sold.flavor)} ${(sold.status = "sold out !")}`;
+    }
+
+});
+console.log(soldOut);
 

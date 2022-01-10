5e46e046f1ec2953baf51233502fe8a846d0dd5f
Nicko <nickos@iMac.local>
2022-01-10 14:26:02 +0100

    Jour07-exo04-start

--- Jour07/Jour07.js
+++ Jour07/Jour07.js
@@ -5,17 +5,17 @@ console.log("Hello World!");
 const fs = require("fs")
 
 fs.readFile("jour07.txt", function (err, data) {
-	if (err) {
-		return console.log("ERROR!!!", err);
-	}
+    if (err) {
+        return console.log("ERROR!!!", err);
+    }
 
-	console.log("Contenu du fichier :", data.toString());
+    console.log("Contenu du fichier :", data.toString());
 });
 
 //exo2
 
 const array = [1, 2, 3, 4, 5];
-const double = array.map (function(num){
+const double = array.map(function (num) {
     return num * 2;
 });
 
@@ -23,3 +23,19 @@ console.log(double);
 
 //exo3
 
+const longNames = [
+    {
+        firstName: "Jane",
+        lastName: "Doe"
+    },
+    {
+        firstName: "John",
+        lastName: "Smith"
+    }
+];
+
+const shortNames = longNames.map(function(longNames){
+        return `${(longNames.firstName)} ${(longNames.lastName)}`
+})
+
+console.log(shortNames);

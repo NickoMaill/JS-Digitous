9c07f2cfb770c07594840ebc8bfc84a9a07bd269
Nicolas-Maillols <nicomaillols@gmail.com>
2022-01-12 10:54:19 +0100

    corrction-jour08

--- Jour08/jour08.js
+++ Jour08/jour08.js
@@ -5,17 +5,13 @@ const axios = require("axios");
 let countriesNames = [];
 
 function getCountries() {
-    axios.get("https://restcountries.com/v3.1/all").then(function (res) {
-
-        countriesNames = res.data.map(function (arrayCountry) {
-            return arrayCountry.name.common;
-        });
-
-        console.log(countriesNames.join(" - "));
-
+  axios.get("https://restcountries.com/v3.1/all").then((res) => {
+    countriesNames = res.data.map((arrayCountry) => {
+      return arrayCountry.name.common;
     });
 
-
+    console.log(countriesNames.join(" - "));
+  });
 }
 
 //getCountries();
@@ -23,23 +19,25 @@ function getCountries() {
 //exo2
 
 function getFact() {
-    axios.get("https://api.chucknorris.io/jokes/random").then(function (res) {
-        console.log(res.data.value);
-    });
-
-};
+  axios.get("https://api.chucknorris.io/jokes/random").then((res) => {
+    console.log(res.data.value);
+  });
+}
 
 //getFact()
 
 //exo3
 
 function catchPokemon(id) {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then(function (res) {
-        console.log("Id :", id);
-        console.log("Name :", res.data.forms[0].name);
-
+  axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((res) => {
+      console.log(`Id : ${id} \nName : ${res.data.forms[0].name}`);
+      
+    })
+    .catch((err) => {
+      console.log(err);
     });
+}
 
-};
-
-catchPokemon(151);
+catchPokemon(898);
